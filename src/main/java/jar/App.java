e3410960ea54414a600a5afcf72406d5b09a7152
Neeraj <ramarajunkodur@gmail.com>
2026-06-02 12:17:23 +0530

    peddi Update App.java

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -5,6 +5,6 @@ package jar;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Neeraj babu!");
+        System.out.println("Peddi! on june 4th");
     }
 }
